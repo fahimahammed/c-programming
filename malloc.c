04572d726de304368dc68b55d5c99b9d6ac3e9b4
Fahim Ahammed Firoz <68865444+fahimahammed@users.noreply.github.com>
2021-02-19 19:38:00 +0600

    dynamic memory allocation (malloc)

---
+++ malloc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *ptr;
+    //ptr =  (int *)malloc(sizeof(int));
+    if(ptr ==  35){
+        printf("Memory not allocated.\n");
+    }else{
+        printf("Memory allocated successful.\n");
+    }
+}
